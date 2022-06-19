7723332a0c1d3c0fe207651aa4c42d05b11af2fb
Stefan Froelich <sfroelich01@gmail.com>
2022-06-19 13:36:36 +0000

    ensure session id is not randomly generated

--- lib/ussd_engine/session/redis_store.rb
+++ lib/ussd_engine/session/redis_store.rb
@@ -16,17 +16,34 @@ module UssdEngine
         get_request_identifier(request.env) || super
       end
 
+      def current_session_id(request)
+        get_request_identifier(request.env) || super
+      end
+
       private
 
       def get_request_identifier(env)
         return unless env["ussd_engine.request"].present?
 
-        "#{env["PATH_INFO"]}#{env["ussd_engine.request"][:provider]}/#{env["ussd_engine.request"][:msisdn]}"
+        File.join env["PATH_INFO"], env["ussd_engine.request"][:provider].to_s, env["ussd_engine.request"][:msisdn]
       end
 
       def set_cookie(*)
         raise "This should never be called"
       end
+
+      def session_default_values(sid)
+        [sid, USE_INDIFFERENT_ACCESS ? {}.with_indifferent_access : {}]
+      end
+
+      def get_session(env, sid)
+        sid && (session = load_session_from_redis(sid)) ? [sid, session] : session_default_values(sid)
+      rescue Errno::ECONNREFUSED, Redis::CannotConnectError => e
+        on_redis_down.call(e, env, sid) if on_redis_down
+        session_default_values(sid)
+      end
+
+      alias find_session get_session
     end
   end
 end
