80dbf9699bfa2763da303ff19057ae7b9f16c7e3
Stefan Froelich <sfroelich01@gmail.com>
2022-08-03 16:08:40 +0000

    safely access env

--- lib/ussd_engine/controller/params.rb
+++ lib/ussd_engine/controller/params.rb
@@ -7,22 +7,32 @@ module UssdEngine
       protected
 
       def ussd_request_id
+        return unless request.env["ussd_engine.request"].present?
+
         request.env["ussd_engine.request"][:id]
       end
 
       def ussd_request_type
+        return unless request.env["ussd_engine.request"].present?
+
         request.env["ussd_engine.request"][:type]
       end
 
       def ussd_request_msisdn
+        return unless request.env["ussd_engine.request"].present?
+
         request.env["ussd_engine.request"][:msisdn]
       end
 
       def ussd_request_provider
+        return unless request.env["ussd_engine.request"].present?
+
         request.env["ussd_engine.request"][:provider]
       end
 
       def ussd_user_input
+        return unless request.env["ussd_engine.request"].present?
+
         request.env["ussd_engine.request"][:input]
       end
     end
